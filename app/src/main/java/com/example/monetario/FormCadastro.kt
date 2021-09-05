bcb85cd1ed33fcd21410d980156560fab3c1325a
Ítalo Santos <italotayllor@gmail.com>
2021-09-05 19:00:16 -0300

    :/ Finalizando UI da tela de cadastro, com o objetivo de realizar-se a integração com o firebase - Android Nativo -

--- app/src/main/java/com/example/monetario/FormCadastro.kt
+++ app/src/main/java/com/example/monetario/FormCadastro.kt
@@ -7,5 +7,7 @@ class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
+
+        getSupportActionBar()?.hide()
     }
 }
--- app/src/main/java/com/example/monetario/FormLogin.kt
+++ app/src/main/java/com/example/monetario/FormLogin.kt
@@ -1,13 +1,22 @@
 package com.example.monetario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class FormLogin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
 
         getSupportActionBar()?.hide()
+
+    }
+    fun onClickCadastro(view: View?) {
+        val intent = Intent(this, FormCadastro::class.java)
+        startActivity(intent)
     }
 }
