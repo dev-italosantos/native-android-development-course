159a6418b20ee43a2d90ef05c5e59f74b38676c4
Ítalo Santos <italotayllor@gmail.com>
2021-09-08 17:08:43 -0300

    :/ Finalizando UI da tela Home, adicionando nome do usuário, logout e bottomNavigation

--- app/src/main/java/com/example/monetario/Home.kt
+++ app/src/main/java/com/example/monetario/Home.kt
@@ -7,5 +7,6 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        supportActionBar?.hide()
     }
 }
