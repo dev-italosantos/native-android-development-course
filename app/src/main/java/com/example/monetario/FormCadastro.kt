3a955ea76054084177ffb0926b239e0fa7eca8ee
Ítalo Santos <italotayllor@gmail.com>
2021-09-07 23:54:46 -0300

    :/ Codificando função para login do usuário no cloud firestore com signInWithEmailAndPassword - Android Nativo -

--- app/src/main/java/com/example/monetario/FormCadastro.kt
+++ app/src/main/java/com/example/monetario/FormCadastro.kt
@@ -22,7 +22,6 @@ class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
-
         supportActionBar?.hide()
 
         auth = FirebaseAuth.getInstance()
--- app/src/main/java/com/example/monetario/FormLogin.kt
+++ app/src/main/java/com/example/monetario/FormLogin.kt
@@ -1,20 +1,94 @@
 package com.example.monetario
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 class FormLogin : AppCompatActivity() {
+    private lateinit var  auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_login)
         supportActionBar?.hide()
 
+        auth = FirebaseAuth.getInstance()
     }
     fun onClickCadastro(@Suppress("UNUSED_PARAMETER")view: View?) {
         val intent = Intent(this, FormCadastro::class.java)
         startActivity(intent)
+    }
+
+    fun onClickEntrar(@Suppress("UNUSED_PARAMETER")view: View?) {
+        val editEmail: EditText = findViewById(R.id.editEmail)
+        val editPassword: EditText = findViewById(R.id.editSenha)
+
+        val email = editEmail.text.toString()
+        val password = editPassword.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            val snack = Snackbar.make(view!!, "This is a simple Snackbar", Snackbar.LENGTH_SHORT)
+
+            snack.view.setBackgroundColor(Color.WHITE)
+            snack.setTextColor(Color.BLACK)
+            snack.show()
+        } else {
+            loginUser(view)
+        }
+    }
+
+    private fun loginUser(view: View?) {
+        val editEmail: EditText = findViewById(R.id.editEmail)
+        val editPassword: EditText = findViewById(R.id.editSenha)
+
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+
+        val email = editEmail.text.toString()
+        val password = editPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                progressBar.visibility = View.VISIBLE
+
+                Handler(Looper.getMainLooper()).postDelayed({
+                    home()
+                }, 3000)
+            } else {
+                val erroAuth: String = try {
+                    throw  task.exception!!
+                } catch (e: FirebaseAuthWeakPasswordException) {
+                    "Enter a password with at least 6 characters"
+                } catch (e: FirebaseAuthInvalidCredentialsException) {
+                    "E-mail invalid"
+                } catch (e: FirebaseNetworkException) {
+                  "Inspect connectivity state"
+                } catch (e: Exception) {
+                    "Error registering user"
+                }
+
+                val snack = Snackbar.make(view!!, erroAuth, Snackbar.LENGTH_SHORT)
+
+                snack.view.setBackgroundColor(Color.WHITE)
+                snack.setTextColor(Color.BLACK)
+                snack.show()
+            }
+        }
+    }
+
+    private fun home() {
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+        finish()
     }
 }
