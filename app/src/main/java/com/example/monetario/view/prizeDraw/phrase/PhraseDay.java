81b42f8a103f24fb76dcffe504de6b4378ffaefd
Ítalo Santos <italotayllor@gmail.com>
2021-11-08 18:13:13 -0300

    feat: desenvolvimento tela de phrase Day :/

---
+++ app/src/main/java/com/example/monetario/view/prizeDraw/phrase/PhraseDay.java
@@ -0,0 +1,15 @@
+package com.example.monetario.view.prizeDraw.phrase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.monetario.R;
+
+public class PhraseDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrase_day);
+    }
+}
