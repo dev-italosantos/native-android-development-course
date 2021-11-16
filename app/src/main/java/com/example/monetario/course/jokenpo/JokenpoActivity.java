92daf80cd0010266818d435c70555fb03bfebb83
Ítalo Santos <italotayllor@gmail.com>
2021-11-16 13:41:32 -0300

    feat: gerar escolha aleatória jogo jokenpô :/

--- app/src/main/java/com/example/monetario/course/jokenpo/JokenpoActivity.java
+++ app/src/main/java/com/example/monetario/course/jokenpo/JokenpoActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.monetario.R;
+
+import java.util.Random;
 
 public class JokenpoActivity extends AppCompatActivity {
 
@@ -21,18 +24,35 @@ public class JokenpoActivity extends AppCompatActivity {
     }
 
     public void selecionaPedra(View view) {
-
+        this.opcaoSelecionada("pedra");
     }
 
     public void selecionaPapel(View view) {
-
+        this.opcaoSelecionada("papel");
     }
 
     public void selecionaTesoura(View view) {
-
+        this.opcaoSelecionada("tesoura");
     }
 
     public void opcaoSelecionada(String opcaoSelecionada) {
+        ImageView imageJokenpo = findViewById(R.id.imageJokenpo);
 
+        int numero = new Random().nextInt(3);
+
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opacaoApp = opcoes[numero];
+
+        switch (opacaoApp) {
+            case "pedra":
+                imageJokenpo.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imageJokenpo.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imageJokenpo.setImageResource(R.drawable.tesoura);
+                break;
+        }
     }
 }
