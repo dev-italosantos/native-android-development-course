4d3e6064e11a6ed5c4eb60f47768786018fe5a60
Ítalo Santos <italotayllor@gmail.com>
2021-12-24 15:29:38 -0300

    feat: implemente nova activity beginnig e configure navegação do app

---
+++ app/src/main/java/com/example/monetario/view/ijon/Beginning.java
@@ -0,0 +1,49 @@
+package com.example.monetario.view.ijon;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.monetario.R;
+
+public class Beginning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginning);
+
+        ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+    }
+
+
+    public void loginIjoin(View view) {
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Beginning.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void registerIjoin(View view) {
+        Button btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Beginning.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
