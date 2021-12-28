3b2a81c3854e396131866807593466502dc17966
Ítalo Santos <italotayllor@gmail.com>
2021-12-28 14:56:12 -0300

    feat: Finalizar layout da classe Alcool ou Gasolina

--- app/src/main/java/com/example/monetario/view/course/alcoholOrGasoline/AlcoholOrGasoline.java
+++ app/src/main/java/com/example/monetario/view/course/alcoholOrGasoline/AlcoholOrGasoline.java
@@ -1,5 +1,6 @@
 package com.example.monetario.view.course.alcoholOrGasoline;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -12,5 +13,9 @@ public class AlcoholOrGasoline extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alcohol_or_gasoline);
+
+        ActionBar actionBar=getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
     }
 }
