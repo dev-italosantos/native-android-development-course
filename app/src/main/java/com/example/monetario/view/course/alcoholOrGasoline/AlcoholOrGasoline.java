46e61f66e433ffc3612e5e48b08be20aac199249
Ítalo Santos <italotayllor@gmail.com>
2021-12-30 13:53:58 -0300

    refactor: refatorar nomes significativos para ids

--- app/src/main/java/com/example/monetario/view/course/alcoholOrGasoline/AlcoholOrGasoline.java
+++ app/src/main/java/com/example/monetario/view/course/alcoholOrGasoline/AlcoholOrGasoline.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.monetario.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class AlcoholOrGasoline extends AppCompatActivity {
+    private TextInputEditText editPriceAlcohol, editPriceGasoline;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,8 @@ public class AlcoholOrGasoline extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
+
+        editPriceAlcohol = findViewById(R.id.editPriceAlcohol);
+        editPriceGasoline = findViewById(R.id.editPriceGasoline);
     }
 }
