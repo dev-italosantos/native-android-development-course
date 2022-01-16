5d68b21d90b485aa6465dbbc95429d400c828f47
Ítalo Santos <italotayllor@gmail.com>
2022-01-16 19:18:27 -0300

    feat: desenvolvar nova activity gogetas calculator

---
+++ app/src/main/java/com/example/monetario/view/course/gogetaCalculator/GogetaCalculator.java
@@ -0,0 +1,16 @@
+package com.example.monetario.view.course.gogetaCalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.monetario.R;
+
+public class GogetaCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gogetas_calculator);
+    }
+}
