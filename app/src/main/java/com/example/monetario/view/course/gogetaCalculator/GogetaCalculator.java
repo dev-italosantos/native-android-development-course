262938a737ee4523da6d9d7dbeebd93b0d173d66
Ítalo Santos <italotayllor@gmail.com>
2022-01-20 16:35:31 -0300

    feat: Adicionar porcentagem gogeta calculator

--- app/src/main/java/com/example/monetario/view/course/gogetaCalculator/GogetaCalculator.java
+++ app/src/main/java/com/example/monetario/view/course/gogetaCalculator/GogetaCalculator.java
@@ -3,14 +3,47 @@ package com.example.monetario.view.course.gogetaCalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.example.monetario.R;
 
 public class GogetaCalculator extends AppCompatActivity {
+    private EditText editValor;
+    private TextView textPorcentagem;
+    private TextView textValorGorjeta;
+    private TextView textValorTotal;
+    private SeekBar seekBarGorjeta;
 
+    private double porcentagem = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gogeta_calculator);
+
+        editValor = findViewById(R.id.editValor);
+        textValorGorjeta = findViewById(R.id.textValorGorjeta);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textValorTotal = findViewById(R.id.textValorTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                porcentagem = progress;
+                textPorcentagem.setText( porcentagem + " %");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
